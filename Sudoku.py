740435e0ee3eab32acf451f5df8ddfbb5be4e8da
Agustin Gauchat <agus123456@hotmail.com>
2019-10-09 15:45:07 -0300

    Sudoku

--- Sudoku.py
+++ Sudoku.py
@@ -1,9 +1,14 @@
-class Sudoku():
+class Sudok():
 
     def __init__(self, tablero):
 
         self.tablero = [[0 for __ in range(9)] for _ in range(9)]
         self.tableroV = tablero
+
+        self.n1 = 0
+        self.n2 = 3
+        self.n3 = 0
+        self.n4 = 3
 
         i = -1
         j = -1
@@ -16,16 +21,66 @@ class Sudoku():
                     self.tablero[i][j] = valor
                 if valor == 'x':
                     self.tablero[i][j] = valor
-        
+
         self.tableroV = self.tablero
 
-    def verificar__valor_fijo(self, x, y):
+    def verificar_x(self):
+        x = 0
+        try:
+            for i in range(self.n3, self.n4):
+                for j in range(self.n1, self.n2):
+                    k = self.tablero[i][j]
+                    if k == 'x':
+                        x += 1
+        except:
+            return
 
-        if self.tableroV[x][y] == 'x':
-            return True
+        if 0 == x:
+            if i == (self.n2 - 1) or self.n2 < 9:
+                self.n1 = self.n1 + 3
+                self.n2 = self.n2 + 3
+            else:
+                self.n1 = 0
+                self.n2 = 3
+                self.n4 = self.n4 + 3
+                if self.n4 > 9:
+                    return
+                else:
+                    self.n3 = self.n3 + 3
+
+    def verificar_bloque(self, num):
+        try:
+            for i in range(self.n3, self.n4):
+                for j in range(self.n1, self.n2):
+                    k = self.tablero[i][j]
+                    if num == k:
+                        return False
+        except:
+            return
+
+    def verificar_fila_columna(self, i, j, num):
+        for columna in range(0, 9):
+            if num == self.tablero[i][columna]:
+                return False
+        for fila in range(0, 9):
+            if num == self.tablero[fila][j]:
+                return False
+
+    def verificar_espacio(self):
+        for i in range(self.n3, self.n4):
+            for j in range(self.n1, self.n2):
+                if self.tablero[i][j] == 'x':
+                    return i, j
+
+    def insertar_numero(self, num):
+        k = self.verificar_espacio()
+        if self.verificar_bloque(num) == None and self.verificar_fila_columna(k[0], k[1], num) == None:
+            self.tablero[k[0]][k[1]] = num
+            self.verificar_x()
+
+            if k == (8, 8):
+                return 'Sudoku completado'
+
+            return "Numero ingresado"
         else:
-            return False
-
-    def verificar_bloque(self, i, j):
-        x = -1
-        y = -1
+            return "Ingrese otro numero"
--- test_sudoku.py
+++ test_sudoku.py
@@ -1,11 +1,11 @@
 import unittest
-from Sudoku import Sudoku_inicio
+from Sudoku import Sudok
 
 
 class TestSudoku(unittest.TestCase):
 
     def setUp(self):
-        self.juego = Sudoku_inicio([
+        self.juego = Sudok([
             '53xx7xxxx',
             '6xx195xxx',
             'x98xxxx6x',
@@ -17,9 +17,71 @@ class TestSudoku(unittest.TestCase):
             'xxxx8xx79'
         ])
 
-    def test_ingresar_numero_correcto(self):
-        
+    def test_fila_incorrecto(self):
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('7'))
 
+    def test_columna_incorrecto(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('4'))
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('7'))
+
+    def test_bloque_incorrecto(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('9'))
+
+    def test_llenar_prox_bloque_fila_incorrecta(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('7'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('4'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('5'))
+
+    def test_llenar_prox_bloque_columna_incorrecta(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('7'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('4'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('8'))
+
+    def test_llenar_prox_bloque_bloque_incorrecta(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('7'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('4'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+        self.assertEqual("Ingrese otro numero", self.juego.insertar_numero('9'))
+
+    def test_llenar_prox_bloque_correcto(self):
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('1'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('7'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('4'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+        self.assertEqual("Numero ingresado", self.juego.insertar_numero('2'))
+
+
+class TestSudokuCompletado(unittest.TestCase):
+
+    def setUp(self):
+        self.juego = Sudok([
+            '531171111',
+            '611195111',
+            '198111161',
+            '811161113',
+            '411813111',
+            '711121116',
+            '161111281',
+            '111419115',
+            '11118117x'
+        ])
+
+    # cambiar n1 = 6 n2 = 9 n3 = 6 n4 = 9 para que tome el ultimo bloque
+
+    def test_fila_incorrecto(self):
+        self.juego.n1 = 6
+        self.juego.n2 = 9
+        self.juego.n3 = 6
+        self.juego.n4 = 9
+        self.assertEqual('Sudoku completado', self.juego.insertar_numero('9'))
 
 
 if __name__ == "__main__":
