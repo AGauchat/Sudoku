749d7a1b3c4391aa730639b3d0b9292eacefd8de
Agustin Gauchat <agus123456@hotmail.com>
2019-10-16 12:11:55 -0300

    Sudoku

--- Interfaz.py
+++ Interfaz.py
@@ -17,16 +17,34 @@ class Interfaz():
         try:
             print("Ingrese fila donde desea poner el numero (0 a 8)")
             f = int(input(">>"))
-            print("Ingrese columna donde desea poner el numero (0 a 8)")
-            c = int(input(">>"))
-            print("Ingrese numero")
-            n = input(">>")
-            if 1 <= n <= 9:
-                print(self.Su.insertar_numero(n, f, c))
+            if self.verificar_fyc(f) is True:
+                print("Ingrese columna donde desea poner el numero (0 a 8)")
+                c = int(input(">>"))
+                if self.verificar_fyc(c) is True:
+                    print("Ingrese numero")
+                    n = int(input(">>"))
+                    if self.verificar_num(n) is True:
+                        print(self.Su.insertar_numero(n, f, c))
+                    else:
+                        print('Ingrese un numero correcto')
+                else:
+                    print('Ingrese un numero correcto')
             else:
                 print('Ingrese un numero correcto')
         except:
             print('Ingrese un numero correcto')
+
+    def verificar_num(self, n):
+        if 1 <= n <= 9:
+            return True
+        else:
+            return False
+
+    def verificar_fyc(self, foc):
+        if 0 <= foc <= 8:
+            return True
+        else:
+            return False
 
     def estado_juego(self):
         if self.Su.gano() is True:
@@ -34,6 +52,6 @@ class Interfaz():
         return False
 
 
-juego = Interfaz()
-while juego.estado_juego() is not True:
-    juego.jugar()
+# juego = Interfaz()
+# while juego.estado_juego() is not True:
+#     juego.jugar()
--- Interfaz_test.py
+++ Interfaz_test.py
@@ -5,8 +5,21 @@ from Interfaz import Interfaz
 
 class Test_Interfaz_Sudoku(unittest.TestCase):
 
-    def test_I(self):
-        return 0
+    def setUp(self):
+        self.int = Interfaz()
+
+    def test_filaocolumna_incorrecta(self):
+        self.assertFalse(self.int.verificar_fyc(23))
+
+    def test_filaocolumna_correcta(self):
+        self.assertTrue(self.int.verificar_fyc(3))
+
+    def test_numero_incorrecto(self):
+        self.assertFalse(self.int.verificar_num(49))
+
+    def test_numero_correcto(self):
+        self.assertTrue(self.int.verificar_num(9))
+
 
 
 if __name__ == "__main__":
